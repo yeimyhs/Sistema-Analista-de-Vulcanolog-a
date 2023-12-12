39a96ccb06fe7599b678cdb521d04c23f7b8d8a6
yeimyhs <yhuancas@unsa.edu.pe>
2023-12-12 22:24:22 +0000

    intento de webpush notif

--- volcanoApp/signals.py
+++ volcanoApp/signals.py
@@ -1,7 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from volcanoApp.models import UserP
+from volcanoApp.models import UserP, Alert
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 
 @receiver(post_save, sender=UserP)
