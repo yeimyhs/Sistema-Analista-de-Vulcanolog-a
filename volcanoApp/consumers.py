cc375a7d7959b392cff2bfd0c673272e05792df7
yeimyhs <yhuancas@unsa.edu.pe>
2023-12-12 22:19:14 +0000

    se crearon los consumers

--- volcanoApp/consumers.py
+++ volcanoApp/consumers.py
@@ -15,3 +15,21 @@ class Tsrealtime(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'message': message
         }))
+
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class NotificacionesConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.volcan_id = self.scope['url_route']['kwargs']['volcan_id']
+        self.group_name = f"volcan_{self.volcan_id}"
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def enviar_mensaje(self, event):
+        mensaje = event['contenido']
+        await self.send(text_data=json.dumps(mensaje))
